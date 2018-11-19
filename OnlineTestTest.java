446f955f033123be6ed62594d17ed6fc01183dcd
Kassim <kassimdia@gmail.com>
2018-11-19 15:03:06 +0100

    Updaterad UnitTesting för OnlineTest samt påbörjan av getQuestions tester.
    Signed-off-by: Kassim <kassimdia@gmail.com>

--- OnlineTestTest.java
+++ OnlineTestTest.java
@@ -10,12 +10,54 @@ public class OnlineTestTest extends TestCase{
 
     public void testOnlineTest() throws Exception {
         OnlineTest test = new OnlineTest("Test of Online Game");
+
         test.current=0;
         test.jb[1].setSelected(true);
-        Assertions.assertTrue(test.current==0&&test.jb[1].isSelected(), "True");
+        Assertions.assertTrue(test.check());
+        System.out.println(test.check());
 
+        test.current=4;
+        test.jb[2].setSelected(true);
+        Assertions.assertTrue(test.check());
+        System.out.println(test.check());
+
+        test.current=7;
+        test.jb[3].setSelected(true);
+        Assertions.assertTrue(test.check());
+        System.out.println(test.check());
+
+        test.current=9;
+        test.jb[1].setSelected(true);
+        Assertions.assertTrue(test.check());
+        System.out.println(test.check());
 
     }
+
+    public void testreturnQuestion() throws Exception {
+       getQuestion test= new getQuestion();
+
+       getQuestion.setArray();
+       Assertions.assertEquals(0,0,getQuestion.returnQuestion(0));
+       System.out.println(getQuestion.returnQuestion(0));
+
+       getQuestion.setArray();
+       Assertions.assertEquals(1,1,getQuestion.returnQuestion(1));
+       System.out.println(getQuestion.returnQuestion(1));
+
+       getQuestion.setArray();
+       Assertions.assertEquals(2,2,getQuestion.returnQuestion(2));
+       System.out.println(getQuestion.returnQuestion(2));
+
+       getQuestion.setArray();
+       Assertions.assertEquals(3,3,getQuestion.returnQuestion(3));
+       System.out.println(getQuestion.returnQuestion(3));
+
+       getQuestion.setArray();
+       Assertions.assertEquals(4,4,getQuestion.returnQuestion(4));
+       System.out.println(getQuestion.returnQuestion(4));
+    }
+
+
 
 
 }
